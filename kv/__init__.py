e82667ca8acc0ad1af1370b64785e4d8aabf8f82
Aatif Syed <aatif@aatifsyed.uk>
2021-12-15 03:51:19 +0000

    simple, append only

--- kv/__init__.py
+++ kv/__init__.py
@@ -1,17 +1,17 @@
+from __future__ import annotations
+
 import argparse
-from argparse import FileType
-from collections import UserDict, defaultdict
-from typing import Any, Callable, Dict, Iterable, Mapping, Sequence, Tuple, TypeVar
-from ZODB.Connection import Connection
-from ZODB.DB import ContextManager as DBContextManager
-import argcomplete
 import logging
-from pathlib import Path
-from logging_actions import log_level_action
-from datetime import datetime
+from collections import UserDict
 from dataclasses import dataclass, field
-import ZODB
-from persistent.mapping import PersistentMapping
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, Optional, Sequence
+
+import argcomplete
+import xdg
+from dataclasses_json import DataClassJsonMixin, config
+from logging_actions import log_level_action
 
 logger = logging.getLogger(__name__)
 
@@ -19,33 +19,65 @@ logger = logging.getLogger(__name__)
 @dataclass
 class Value:
     value: str
-    created: field(default_factory=datetime.now)
-    comment: str
+    created: datetime = field(
+        default_factory=datetime.now,
+        metadata=config(encoder=datetime.isoformat, decoder=datetime.fromisoformat),
+    )
+    comment: Optional[str] = None
 
 
-def main():
+@dataclass
+class KV(DataClassJsonMixin):
+    mapping: Dict[str, Value]
+
+
+class LenientChoices(UserDict):
+    def __contains__(self, key: object) -> bool:
+        """Return True so that argparse will validate any key as a choice"""
+        return True
+
+
+def main(
+    kv_file: Path = xdg.xdg_data_home().joinpath("kv.json"),
+    args: Optional[Sequence[str]] = None,
+):
     logger.addHandler(logging.StreamHandler())
+    logger.debug(f"{kv_file=}")
 
-    conn: Connection
-    with ZODB.DB("db").transaction() as conn:
-        try:
-            mapping: Mapping[str, Value] = conn.root.mapping
-        except AttributeError:
-            logger.info(f"Initializing database {conn}")
-            conn.root.mapping = PersistentMapping()
-            mapping: Mapping[str, Value] = conn.root.mapping
-
-        parser = argparse.ArgumentParser(description="description")
-        parser.add_argument(
-            "-l",
-            "--log-level",
-            action=log_level_action(logger),
-            default="info",
+    try:
+        kv = KV.from_json(kv_file.read_text())
+    except Exception as e:
+        logger.error(
+            f"Unable to load from {kv_file} ({e}), ignoring and using a new kv store",
         )
-        parser.add_argument(
-            "key", choices=defaultdict(None, mapping)
-        )  # Default because we want to accept new keys, but still get autocompletion for old ones
-        parser.add_argument("value", nargs="?")
-        args = parser.parse_args()
+        kv = KV(mapping={})
 
-        logger.debug(f"{args=}")
+    parser = argparse.ArgumentParser(
+        description="A persistent key-value store for the shell"
+    )
+    parser.add_argument(
+        "--log-level",
+        action=log_level_action(logger),
+        default="info",
+    )
+    parser.add_argument(
+        "key",
+        choices=LenientChoices(kv.mapping),
+        metavar="key",
+        help="Retrive the value of this key",
+    )
+    parser.add_argument("value", nargs="?", help="If specified, set key to value")
+
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args(args=args)
+
+    logger.debug(f"{args=}")
+
+    if args.value is None:
+        # This is a get. Print out
+        print(kv.mapping[args.key].value)
+    else:
+        # This is a set. Modify and save
+        kv.mapping[args.key] = Value(value=args.value)
+        kv_file.parent.mkdir(parents=True, exist_ok=True)
+        kv_file.write_text(kv.to_json())
